c708e4f886a2a419d9d15a201032c117ed2d196f
CodenificenT <61930093+siechristian@users.noreply.github.com>
2020-06-23 18:31:50 -0400

    learned chapter 3

---
+++ Exercise Files/Ch03/03-13_evaluate.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void evaluate(int x);
+int main()
+{
+	int i;
+
+	printf("Type an integer value: ");
+	scanf("%d",&i);
+	evaluate(i);
+
+	return(0);
+}
+
+void evaluate(int x)
+{
+	/* insert code here */
+
+	if (x < 10)
+        printf(" %d is less than 10\n", x);
+    else if ( x > 10)
+        printf(" %d is greater than 10\n", x);
+    else
+        printf(" %d is equal to 10\n", x);
+}
+
